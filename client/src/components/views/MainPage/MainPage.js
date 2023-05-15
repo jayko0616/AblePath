03802e373efacf78200697f6d85caa53fdd72b79
Ko Jaehwi <kojaehwi@gmail.com>
2023-05-15 16:59:33 +0900

    map_mainpage_Ko

--- client/src/components/views/MainPage/MainPage.js
+++ client/src/components/views/MainPage/MainPage.js
@@ -1,20 +1,33 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 
+const {kakao} = window;
 
 function MainPage() {
+
+  useEffect(() => {
+    // API 로딩이 완료되면 지도를 초기화합니다.
+      const container = document.getElementById('map'); //지도 담을 영역의 DOM 레퍼런스
+      const options = {
+        center: new kakao.maps.LatLng(33.450701, 126.570667),   //지도 중심 좌표
+        level: 3
+      };
+      const map = new kakao.maps.Map(container, options);   //지도 생성 및 객체 리턴;
+  }, []);
 
   return (
     <div>
       <Header/>
       <body>
-        <h1>메인 페이지</h1>
-        <div>main page testing ... </div>
+        <h1>Main Page</h1>
+            <div style={{width: '100%', display: 'inline-block', marginLeft: '5px', marginRight: '5px',}}>
+            <div id="map" style={{ width: '99%', height: '500px' }}></div>
+        </div>
       </body>
       <Footer/>
     </div>
   )
 }
 
-export default MainPage;
+export default MainPage

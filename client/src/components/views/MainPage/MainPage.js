b760b28b30d13357f0bb57a4fc7a9a901d8d10e0
Ko Jaehwi <kojaehwi@gmail.com>
2023-05-16 12:38:38 +0900

    searchBtn_Ko

--- client/src/components/views/MainPage/MainPage.js
+++ client/src/components/views/MainPage/MainPage.js
@@ -1,6 +1,7 @@
-import React, {useEffect} from 'react'
+import React, {useEffect ,useState} from 'react'
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
+import "../MainPage/MainPage.css";
 
 const {kakao} = window;
 
@@ -16,11 +17,30 @@ function MainPage() {
       const map = new kakao.maps.Map(container, options);   //지도 생성 및 객체 리턴;
   }, []);
 
+  const [showPopup, setShowPopup] = useState(false);
+  
+  const togglePopup = () => {
+    setShowPopup(!showPopup);
+  };
+
   return (
-    <div>
+    <div className='center'>
       <Header/>
       <body>
-        <h1>Main Page</h1>
+        <h1>
+          Main Page 
+        </h1>
+        <h1 className='right'>
+          <div>
+            <button className='searchBtn' onClick={togglePopup}>Search</button>
+            {showPopup && (
+            <div className="popup">
+            <h1>팝업 내용</h1>
+            <button onClick={togglePopup}>팝업 닫기</button>
+        </div>
+      )}
+          </div>
+        </h1>
             <div style={{width: '100%', display: 'inline-block', marginLeft: '5px', marginRight: '5px',}}>
             <div id="map" style={{ width: '99%', height: '500px' }}></div>
         </div>
@@ -30,4 +50,4 @@ function MainPage() {
   )
 }
 
-export default MainPage
+export default MainPage;

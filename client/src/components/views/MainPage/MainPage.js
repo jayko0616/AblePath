4612e20ba7e789c79419e1244a82943c4c8f5c44
Ko Jaehwi <kojaehwi@gmail.com>
2023-05-17 13:01:18 +0900

    searchPopup_Ko

--- client/src/components/views/MainPage/MainPage.js
+++ client/src/components/views/MainPage/MainPage.js
@@ -1,21 +1,24 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect} from 'react';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 import '../MainPage/MainPage.css';
 
 const { kakao } = window;
 
-function MainPage() {
+function initializeMap() {
+  const container = document.getElementById('map'); // 지도 담을 영역의 DOM 레퍼런스
+  const options = {
+    center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도 중심 좌표
+    level: 3,
+  };
+  const map = new kakao.maps.Map(container, options); // 지도 생성 및 객체 리턴;
+}
+
+function MainPage() {  
     useEffect(() => {
-      // API 로딩이 완료되면 지도를 초기화합니다.
-      const container = document.getElementById('map'); //지도 담을 영역의 DOM 레퍼런스
-      const options = {
-        center: new kakao.maps.LatLng(33.450701, 126.570667), //지도 중심 좌표
-        level: 3,
-      };
-      const map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴;
+      initializeMap();
     }, []);
-  
+
     const openModal = () => {
       setIsOn(true);
     };
@@ -25,7 +28,6 @@ function MainPage() {
     };
   
     function renderModal() {
-
       return (
         <>
           <div className="dim"> </div>

de6e5976917c31a9c0f3438f3029f125d4706eaa
Jeong Dahyun <tv11111@naver.com>
2023-05-30 00:08:38 +0900

    server-client communication _jeong

--- client/src/_actions/data_action.js
+++ client/src/_actions/data_action.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export function get_bus(dataToSubmit){
-    const request = axios.get('/info/bus/get', dataToSubmit)
+    const request = axios.post('/info/bus/get', dataToSubmit)
     .then(response => response.data)
 
     return{
--- server/data/Bus/lowbus.js
+++ server/data/Bus/lowbus.js
@@ -5,12 +5,13 @@ const key = require('../../config/api_key')
 
 
 
-export function get_stId(){
+async function get_stId(){
     const baseURL = 'http://ws.bus.go.kr/api/rest/arrive/getLowArrInfoByStId?ServiceKey=';
     const stId = 112000001;
     var url;
     url = baseURL+ key.bus_key + '&stId=' + stId;
 
+    /*
     axios({
         method : 'get',
         url : url,
@@ -27,5 +28,33 @@ export function get_stId(){
             console.log("");
         });
     })
+    */
+
+    try{
+        const res = await axios.get(url);
+        
+        console.log('Status', res.status, res.statusText);
+        var xml = res.data;
+        var json = convert.xml2json(xml, {compact: true});  //xml -> json
+        const items = JSON.parse(json);     //js에서 바로 변수로 사용할 수 있도록
+        const base = items.ServiceResult.msgBody.itemList;
+        Object.keys(base).forEach(function(i) {
+            console.log("버스 번호", base[i].busRouteAbrv._text);
+            console.log("남은 시간1", base[i].arrmsg1._text);
+            console.log("남은 시간2", base[i].arrmsg2._text);
+            console.log("");
+        });
+
+        return {
+            getSuccess: true
+            
+        }
+    }
+    catch(err) {
+        console.log("Failed to get info about lowbus.")
+    }
+
 
 }
+
+module.exports.get_stId = get_stId;
